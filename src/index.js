28d898c63ecb95089fb3c46c0796f9b4b85081ff
yersel <yersel500@gmail.com>
2022-02-17 12:27:06 -0500

    Add task with an input

--- src/index.js
+++ src/index.js
@@ -1,31 +1,100 @@
 import './style.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
+// Tasks container
 const ulContainer = document.querySelector('.list');
+// Input of tasks
+const taskInput = document.querySelector('.task-input');
 
-const lisTasks = [
-  {
-    description: 'task 1',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'task 2',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'task 3',
-    completed: false,
-    index: 2,
-  },
-];
+//Events
+eventListeners();
+function eventListeners() {
+  taskInput.addEventListener('input', saveTask);
+}
 
-window.onload = () => {
-  lisTasks.forEach((task) => {
-    const taskContainer = document.createElement('li');
-    taskContainer.classList.add(`${task.index}`, 'list-unit');
-    taskContainer.innerHTML = `<i class="fa-regular fa-square"></i> ${task.description} <i class="fas fa-ellipsis-v dots"></i>`;
-    ulContainer.appendChild(taskContainer);
-  });
-};
+// Empty object for task
+const objTask = {
+  description: '',
+  complete: ''
+}
+
+function saveTask(e) {
+  objTask[e.target.name] = e.target.value;
+  objTask.complete = false;
+  console.log(e.target.value);
+}
+
+//class for array of objects
+class ArrayTask {
+  constructor() {
+    this.tasks = []
+  }
+
+  addTask(task) {
+    this.tasks = [...this.tasks, task];
+  }
+
+  editTask(updatedTask) {
+    this.tasks = this.tasks.map( task => task.index === updatedTask.index ? updatedTask: task);
+  }
+
+  deleteTask(index) {
+    this.tasks = this.task.filter( task => task.index !== index);
+  }
+}
+
+// class for UI
+
+class UI {
+  cleanHTML() {
+    while(ulContainer.firstChild) {
+      ulContainer.removeChild(ulContainer.firstChild);
+    }
+  }
+  printTask({tasks}) {
+
+    this.cleanHTML();
+
+    tasks.forEach(task => {
+      const {description, complete, index} = task;
+
+      const taskContainer = document.createElement('li');
+      taskContainer.classList.add(`${index}`, 'list-unit', `${complete}`);
+      taskContainer.innerHTML = `<i class="fa-regular fa-square"></i> ${description} <i class="fas fa-ellipsis-v dots"></i>`;
+      ulContainer.appendChild(taskContainer);
+
+    });
+    
+
+  }
+}
+
+const ui = new UI();
+const listofTasks = new ArrayTask();
+
+// Add task
+
+function newTask(e) {
+  const {description, complete} = objTask;
+  if (e.key == 'Enter' && description !== '') {
+    if(complete) {
+
+    } else {
+      objTask.index = listofTasks.length;
+      listofTasks.addTask({...objTask});
+    }
+
+    ui.printTask(listofTasks);
+
+    resetObject();
+  
+    taskInput.value = '';
+  }
+}
+
+function resetObject() {
+  objTask.description = '';
+}
+
+document.addEventListener('keypress', newTask);
+
